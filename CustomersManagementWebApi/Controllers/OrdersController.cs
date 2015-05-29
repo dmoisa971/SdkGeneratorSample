9b40ee984f62b7f602f865f1fc152cd36b3b564e
t170975 <david.moisa@ai3.fr>
2015-05-29 10:41:11 +0200

    Project Initialisation

---
+++ CustomersManagementWebApi/Controllers/OrdersController.cs
@@ -0,0 +1,42 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CustomersManagementWebApi.Controllers
+{
+    public class OrdersController : ApiController
+    {
+        // GET: api/Orders
+        public IEnumerable<Order> Get()
+        {
+            var orders = new List<Order>();
+
+            return orders;
+        }
+
+        // GET: api/Orders/5
+        public Order GetById(int id)
+        {
+            return new Order();
+        }
+
+        // POST: api/Orders
+        public void Post([FromBody]Order value)
+        {
+        }
+
+        // PUT: api/Orders/5
+        public void Put(int id, [FromBody]Order value)
+        {
+        }
+
+        // DELETE: api/Orders/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Model/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Model
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public string Adresse { get; set; }
+        public string Phone { get; set; }
+    }
+}
