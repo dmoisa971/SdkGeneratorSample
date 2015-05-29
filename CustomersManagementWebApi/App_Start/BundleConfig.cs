3e07ff58865406a7a6c554373286af2185ba1c87
dmoisa971 <david_moisa@hotmail.com>
2015-05-29 11:36:25 +0200

    Revert "Revert "Project Initialisation""
    This reverts commit 055aa981a5a52c709812c654c80db2e5377ee9f9.

---
+++ CustomersManagementWebApi/App_Start/BundleConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace CustomersManagementWebApi
+{
+    public class BundleConfig
+    {
+        // Pour plus d’informations sur le regroupement, rendez-vous sur http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+
+        }
+    }
+}
